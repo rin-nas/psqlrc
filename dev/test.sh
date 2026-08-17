c966c69d3db70bf56d773418bb3b337c9421aaa2
rin-nas <rin-nas@ya.ru>
2026-08-17 21:59:38 +0300

    tests updated

--- dev/test.sh
+++ dev/test.sh
@@ -1,15 +1,13 @@
+psql -v pro=install -c ''
 psql -v pro=install -c '' biha_db
 
-psql -v pro=0 -q -c '\echo :W :A' biha_db | psql -v pro=0 -q biha_db
-
-psql -v pro=0 -q -c '\echo :T :B :BB' biha_db | psql -v pro=0 -q biha_db
+psql -v pro=0 -q -c '\echo :W :A' | psql -v pro=0 -q
+psql -v pro=0 -q -c '\echo :T' | psql -v pro=0 -q
+psql -v pro=0 -q -c '\echo :B :BB' biha_db | psql -v pro=0 -q biha_db
 
 #-----------------------------------------------------------------------------------------------------------------------
 
 psql -v pro=0 -q
-
 psql -v pro=0
-
 psql -v pro=1
-
 psql -v pro=color
