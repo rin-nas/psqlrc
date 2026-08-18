8c5dd99bc25b541dabd31c848e77de132b22965c
rin-nas <rin-nas@ya.ru>
2026-08-18 10:59:58 +0300

    install: dblink(..., fail_on_error=true) ; view `pro.pg_cluster_topology` test disabled

--- dev/test.sh
+++ dev/test.sh
@@ -1,14 +1,14 @@
 psql -v pro=0 -q -f ~/psqlrc/command_INSTALL.psql biha_db
-psql -v pro=0 -q -f ~/psqlrc/command_UNINSTALL.psql biha_db
 psql -v pro=0 -q -f ~/psqlrc/command_REINSTALL.psql biha_db
+psql -v pro=0 -q -f ~/psqlrc/command_UNINSTALL.psql biha_db
 
-psql -v pro=0 -q -c '\echo :W :A' | psql -v pro=0 -q
-psql -v pro=0 -q -c '\echo :T' | psql -v pro=0 -q
+psql -v pro=0 -q -c '\echo :W :A' | psql -v pro=0 -q biha_db
+psql -v pro=0 -q -c '\echo :T' | psql -v pro=0 -q biha_db
 psql -v pro=0 -q -c '\echo :B :BB' biha_db | psql -v pro=0 -q biha_db
 
 #-----------------------------------------------------------------------------------------------------------------------
 
-psql -v pro=0 -q
-psql -v pro=0
-psql -v pro=1
-psql -v pro=color
+psql -v pro=0 -q biha_db
+psql -v pro=0 biha_db
+psql -v pro=1 biha_db
+psql -v pro=color biha_db
