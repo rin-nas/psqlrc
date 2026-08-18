f0def4a0963993c83995f0be5619f8cb04dc2105
rin-nas <rin-nas@ya.ru>
2026-08-18 16:08:26 +0300

    short commands set title improved; tests improved

--- dev/test.sh
+++ dev/test.sh
@@ -2,9 +2,10 @@ psql -v pro=0 -q -f ~/psqlrc/command_INSTALL.psql biha_db
 psql -v pro=0 -q -f ~/psqlrc/command_REINSTALL.psql biha_db
 psql -v pro=0 -q -f ~/psqlrc/command_UNINSTALL.psql biha_db
 
-psql -v pro=0 -q -c '\echo :W :A' | psql -v pro=0 -q biha_db
-psql -v pro=0 -q -c '\echo :T' | psql -v pro=0 -q biha_db
-psql -v pro=0 -q -c '\echo :B :BB' biha_db | psql -v pro=0 -q biha_db
+psql -v pro=0 -q -f ~/psqlrc/command_W.psql biha_db
+psql -v pro=0 -q -f ~/psqlrc/command_A.psql biha_db
+psql -v pro=0 -q -f ~/psqlrc/command_B.psql biha_db
+psql -v pro=0 -q -f ~/psqlrc/command_BB.psql biha_db
 
 #-----------------------------------------------------------------------------------------------------------------------
 
