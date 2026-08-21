f488f93044910daaa52e6d505f5f8d4e9407a525
rin-nas <rin-nas@ya.ru>
2026-08-21 16:13:20 +0300

    test.sh updated

--- dev/test.sh
+++ dev/test.sh
@@ -1,19 +1,14 @@
-chown -R postgres: ~/psqlrc
-chmod -R 600 ~/psqlrc
-chmod 700 ~/psqlrc
-
-psql -v pro=0 -q -f ~/psqlrc/command_INSTALL.psql biha_db
-psql -v pro=0 -q -f ~/psqlrc/command_REINSTALL.psql biha_db
-psql -v pro=0 -q -f ~/psqlrc/command_UNINSTALL.psql biha_db
-
-psql -v pro=0 -q -f ~/psqlrc/command_W.psql biha_db | sed 's/↵/ /g'
-psql -v pro=0 -q -f ~/psqlrc/command_A.psql biha_db | sed 's/↵/ /g'
-psql -v pro=0 -q -f ~/psqlrc/command_B.psql biha_db | sed 's/↵/ /g'
-psql -v pro=0 -q -f ~/psqlrc/command_BB.psql biha_db | sed 's/↵/ /g'
-
-#-----------------------------------------------------------------------------------------------------------------------
+# интерактивный режим с приглашением командной строки
 
 psql -v pro=0 -q biha_db
 psql -v pro=0 biha_db
 psql -v pro=1 biha_db
 psql -v pro=color biha_db
+
+# встраивание в bash скрипты
+
+psql -v pro=0 -q -f ~/psqlrc/command_W.psql biha_db | sed 's/↵/ /g'
+psql -v pro=0 -q -f ~/psqlrc/command_A.psql biha_db | sed 's/↵/ /g'
+psql -v pro=0 -q -f ~/psqlrc/command_T.psql biha_db | sed 's/↵/ /g'
+psql -v pro=0 -q -f ~/psqlrc/command_B.psql biha_db | sed 's/↵/ /g'
+psql -v pro=0 -q -f ~/psqlrc/command_BB.psql biha_db | sed 's/↵/ /g'
